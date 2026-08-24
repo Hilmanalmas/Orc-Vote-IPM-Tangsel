ccc56b077ec5174c422b84f83a145e25b3c33873
Your Name <you@example.com>
2026-08-25 02:19:21 +0700

    Update database credentials based on wp-config

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Database Configuration
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'localhost');
 define('DB_NAME', 'ipm_voting');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_USER', 'ipm_user');
+define('DB_PASS', 'ipm_password');
 
 try {
     $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
