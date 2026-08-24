9d79f3dff4b9c3ed890639338a0b51219b6bbe87
Your Name <you@example.com>
2026-08-25 01:47:21 +0700

    Fix database connection issue by using 127.0.0.1

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Database Configuration
-define('DB_HOST', 'localhost');
+define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'ipm_voting');
 define('DB_USER', 'root');
 define('DB_PASS', '');
