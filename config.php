891dbee1b17c21834b943f2c97469c264c476287
Your Name <you@example.com>
2026-08-25 02:32:23 +0700

    Update DB_HOST for Docker container ipm-db

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Database Configuration
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'ipm-db');
 define('DB_NAME', 'ipm_voting');
-define('DB_USER', 'ipm_user');
-define('DB_PASS', 'ipm_password');
+define('DB_USER', 'root');
+define('DB_PASS', '');
 
 try {
     $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
